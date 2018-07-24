09dfbff4f20f921b88deb16be782ced37af09cee
Kapitoshkas <moiseenkoartem@yandex.ru>
2018-07-24 16:47:26 +0300

    Добавлен тест на модификацию ContactModification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -50,4 +50,12 @@ public class ContactHelper extends Helperbase {
     wd.switchTo().alert().accept();
   }
 
+  public void initContactModification (){
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void submitContactModification() {
+    click(By.xpath("//div[@id='content']/form[1]/input[22]"));
+  }
+
   }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification() {
+   app.getContactHelper().initContactModification();
+   app.getContactHelper().fillNewContactForm(new ContactData("first name", "middle name", "last name", "new title", "new company", "first address", "first home", "first mobile", "first work", "first mail", "second mail", "1900", "second address", "second home"));
+   app.getContactHelper().submitContactModification();
+  }
+  
+}
