bed766d6a3be0ba5e0ffc8b1527849bc8346e7ab
Kapitoshkas <moiseenkoartem@yandex.ru>
2018-08-08 10:32:03 +0300

    Выбор элемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test33", "test44", "test55"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before -1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test33", "test44", "test55"));
     app.getGroupHelper().submitGroupModification();
