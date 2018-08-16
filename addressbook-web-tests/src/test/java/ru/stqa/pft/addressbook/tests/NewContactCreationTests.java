2c941c09035a643341b66983e6c3d35040f1877f
Kapitoshkas <moiseenkoartem@yandex.ru>
2018-08-16 10:27:26 +0300

    Запуск браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/NewContactCreationTests.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class NewContactCreationTests extends TestBase{
 
 
-  @Test
+  @Test (enabled = false)
   public void testNewContactCreation() {
     app.getNavigationHelper().returnToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
