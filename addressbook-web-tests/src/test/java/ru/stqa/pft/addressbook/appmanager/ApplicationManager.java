dec1686a18a301053fd84bba897fbba1af201a9a
Kapitoshkas <moiseenkoartem@yandex.ru>
2018-08-27 13:54:45 +0300

    Сравнение данных, загруженных из БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser)  {
     this.browser = browser;
@@ -34,6 +35,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    dbHelper = new DbHelper();
 
       if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
@@ -50,7 +53,7 @@ public class ApplicationManager {
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
-  }
+      }
 
 
   public void stop() {
@@ -77,5 +80,9 @@ public class ApplicationManager {
 
   public void returnToHomePage() {
     navigationHelper.returnToHomePage();
+  }
+
+  public DbHelper db() {
+    return dbHelper;
   }
 }
