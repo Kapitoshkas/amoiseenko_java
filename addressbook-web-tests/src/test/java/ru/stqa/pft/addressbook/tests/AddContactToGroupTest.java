fca59899e5b2ce9ae964e960e8cf7443472becdc
Kapitoshkas <moiseenkoartem@yandex.ru>
2018-08-30 15:18:08 +0300

    Задание 16. Тесты добавления/удаления группы из контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactToGroupTest.java
@@ -0,0 +1,51 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AddContactToGroupTest extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test1").withHeader("Header").withFooter("Footer"));
+    }
+  }
+
+  @Test
+  public void testAddContactToGroup() {
+    ContactData addingToGroupContact = new ContactData().withName("first name")
+            .withLastName("last name")
+            .withAddress("Moscow")
+            .withMobilePhone("+71234567890")
+            .withEmail("test@test.com");
+    app.gotoContactPage();
+    app.contact().createContact(addingToGroupContact);
+
+    Groups beforeGroups = app.db().groups();
+    GroupData groupForAddingContact = beforeGroups.iterator().next();
+    app.returnToHomePage();
+
+    int id = app.db().getContactLastId(addingToGroupContact);
+    app.contact().selectContactById(id);
+    app.group().selectGroupToIncludeContact(groupForAddingContact);
+    app.contact().addContactToGroup();
+    Contacts afterContacts = app.db().contacts();
+    ContactData contactToCompare = new ContactData();
+    for(ContactData contact: afterContacts){
+      if (contact.getId() == id){
+        contactToCompare = contact;
+        break;
+      }
+    }
+    assertThat(contactToCompare.getGroups(), equalTo(addingToGroupContact.inGroup(groupForAddingContact).getGroups()));
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/RemoveContactFromGroupTests.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RemoveContactFromGroupTests extends TestBase{
+  @BeforeMethod
+  public void ensurePreconditions() {
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test1").withHeader("Header").withFooter("Footer"));
+    }
+  }
+
+  @Test
+  public void testRemoveContactFromGroup() {
+    Groups groups = app.db().groups();
+    GroupData ourGroup = groups.iterator().next(); //- любая, и мы ее делаем нашей =).
+    app.returnToHomePage();
+    ContactData removingContact = new ContactData().withName("first")
+            .withLastName("last name")
+            .withAddress("Moscow")
+            .withMobilePhone("+71234567890")
+            .withEmail("test@test.com").inGroup(ourGroup);
+    app.gotoContactPage();
+    app.contact().createContact(removingContact);
+
+    int id = app.db().getContactLastId(removingContact);
+    app.group().selectGroupToCheckIncludedContacts(ourGroup);
+    app.contact().selectContactById(id);
+    app.contact().removeContactFromGroup();
+    ContactData contactToCompare = new ContactData();
+    Contacts afterContacts = app.db().contacts();
+    for(ContactData contact: afterContacts){
+         if (contact.getId() == id){
+        contactToCompare = contact;
+        break;
+      }
+    }
+        assertThat(contactToCompare.inGroup(ourGroup).getGroups(), equalTo(removingContact.getGroups()));
+  }
+}
