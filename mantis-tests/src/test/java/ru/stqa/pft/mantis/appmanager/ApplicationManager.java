ae710a78dca906f36978b37598071129445f84f9
Kapitoshkas <moiseenkoartem@yandex.ru>
2018-08-31 15:27:07 +0300

    Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -23,6 +23,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
 
   public ApplicationManager(String browser)  {
@@ -81,5 +82,11 @@ public class ApplicationManager {
     }
 
     return wd;
+  }
+  public MailHelper mail() {
+    if (mailHelper  == null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
   }
 }
