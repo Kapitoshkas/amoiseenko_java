39941b12468abded1d0cfa19ce0cb14ba87a225c
Kapitoshkas <moiseenkoartem@yandex.ru>
2018-09-05 15:43:04 +0300

    Задание 18 +проверка БД

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTests.java
@@ -1,12 +1,17 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.appmanager.DbHelper;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
 import ru.stqa.pft.mantis.model.MailMessage;
+import ru.stqa.pft.mantis.model.UserData;
+
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import static org.testng.Assert.assertTrue;
 
 
@@ -16,8 +21,13 @@ public class ChangePasswordTests extends TestBase {
   @Test
   public void changePassword() throws IOException, MessagingException {
 
-    String email = "user1535973450787@localhost";
-    String username = "user1535973450787";
+    List<UserData> users = new DbHelper().users();
+    int index = 0;
+    do{
+      index = (int) new Random().nextInt(users.size());
+    } while (users.get(index).getUser().equals("administrator"));
+    String email = users.get(index).getEmail();
+    String username = users.get(index).getUser();
     String password = "password";
     List<MailMessage> mailMessagesBefore = app.james().waitForMail(username,password,60000);
 
